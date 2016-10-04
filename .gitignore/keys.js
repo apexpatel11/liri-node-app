fb3faf1a5ef79b18fe29883f152ddbbb59cf3084
apex patel <apexa0630@outlook.com>
2016-10-04 09:49:25 -0500

    changes done

--- .gitignore/keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: '<lp13rUDMPuCGfrKuyayRaGeu6>',
-  consumer_secret: '<IWNWtRRf3pPWDN3cRWz58yUpik5LdMzfKnF5FBNaWVgu0W3cMM>',
-  access_token_key: '<4702434110-7O182CgiKXvKR71k2e7TnU0cXqwpIZ0T6QPkWfX>',
-  access_token_secret: '<6xeRIUOx24hFkIpfxcwMSr7xhXfSDLGN7ot9dd7dROMzI>',
-}
